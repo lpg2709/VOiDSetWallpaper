42394fae7d66a74b03244288489a38833ad65454
lpg2709 <leonardo_guarezi@hotmail.com>
2022-07-09 20:51:46 -0300

    [ADD] Read wallpaper information; [ADD] Set new Wallpaper

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,18 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <stdlib.h>
+
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDir>
+#include <QPixmap>
+#include <QString>
+#include <QVector>
+#include <windows.h>
+#include <QMessageBox>
+
+#include "windowsreg.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,13 +25,18 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    bool isFileSelected;
+
+    enum WALLPAPER_STYLE {
+        CENTER, TILE, STRETCH, FIT, FILL, SPAN
+    };
 
 private slots:
-    void on_pushButton_set_clicked();
-
     void on_pushButton_tab1_open_file_clicked();
 
     void get_current_wallpaper();
+
+    void on_pushButton_tab1_setwallpaper_clicked();
 
 private:
     Ui::MainWindow *ui;
---
+++ windowsreg.cpp
@@ -0,0 +1,31 @@
+#include "windowsreg.h"
+
+WindowsReg::WindowsReg()
+{
+
+}
+
+int WindowsReg::get_REG_SZ_value(HKEY _hkey, LPCSTR _lpSubKey, LPCSTR _lpValue){
+    // Get Wallpaper Style
+    HKEY hkey = _hkey;
+    LPCSTR lpSubKey = _lpSubKey;
+    LPCSTR lpValue = _lpValue;
+    DWORD dwFlags = RRF_RT_REG_SZ;
+    LPDWORD pdwType = nullptr;
+    DWORD val;
+    DWORD dataSize = sizeof(val);
+
+    int result = RegGetValueA(hkey, lpSubKey, lpValue, dwFlags, pdwType, &val, &dataSize);
+
+    if (ERROR_SUCCESS == result) {
+        return val - 48;
+    } else {
+        return result;
+    }
+}
+
+/*
+int WindowsReg::get_DWORD_value(HKEY hkey, LPCSTR lpSubKey, LPCSTR lpValue){
+
+}
+*/
