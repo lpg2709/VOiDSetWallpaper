ff19d02e8e79d535debef7a59076d2cf46515926
lpg2709 <leonardo_guarezi@hotmail.com>
2022-07-09 20:51:49 -0300

    [FIX] Set Registry for all options

--- windowsreg.h
+++ windowsreg.h
@@ -2,6 +2,8 @@
 #define WINDOWSREG_H
 
 #include <windows.h>
+#include <QString>
+#include <QStringList>
 
 class WindowsReg
 {
@@ -9,9 +11,9 @@ public:
     WindowsReg();
 
     static int get_REG_SZ_value(HKEY hkey, LPCSTR lpSubKey, LPCSTR lpValue);
-    static int set_REG_SZ_value(HKEY hKey, LPCSTR lpValueName, DWORD data);
+    static int set_REG_SZ_value(HKEY _hKey, LPCSTR strSubKey, LPCSTR lpVal, DWORD _lpData);
     static int get_DWORD_value(HKEY hkey, LPCSTR lpSubKey, LPCSTR lpValue);
-    static int set_DWORD_value(HKEY hKey, LPCSTR lpValueName, DWORD data);
+    static int set_DWORD_value(HKEY _hKey, LPCSTR strSubKey, LPCSTR lpVal, DWORD _lpData);
 };
 
 #endif // WINDOWSREG_H
