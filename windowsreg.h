e38bc80fbee5e74b1d51ca7bd5e4fab629823a6d
lpg2709 <leonardo_guarezi@hotmail.com>
2022-07-09 20:51:51 -0300

    [ADD] logs tab; [ADD] Copy to default path;

--- windowsreg.h
+++ windowsreg.h
@@ -9,7 +9,7 @@ public:
     WindowsReg();
 
     static int get_REG_SZ_value(HKEY hkey, LPCSTR lpSubKey, LPCSTR lpValue);
-    static int set_REG_SZ_value(HKEY hkey, LPCSTR lpSubKey, LPCSTR lpValue);
+    static int set_REG_SZ_value(HKEY _hKey, LPCSTR _lpValueName, const BYTE *_lpData);
 
     // static int get_DWORD_value(HKEY hkey, LPCSTR lpSubKey, LPCSTR lpValue);
 };
